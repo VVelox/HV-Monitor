625c6809ab6949671b9cfda34f24b42f230dbc87
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-06-26 07:50:20 -0500

    add initial bits

---
+++ lib/HV/Monitor.pm
@@ -0,0 +1,105 @@
+package HV::Monitor;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+HV::Monitor - The great new HV::Monitor!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use HV::Monitor;
+
+    my $foo = HV::Monitor->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-hv-monitor at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=HV-Monitor>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc HV::Monitor
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=HV-Monitor>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/HV-Monitor>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/HV-Monitor>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2022 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of HV::Monitor
