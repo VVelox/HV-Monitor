166a2bce67fb78355eaff43c3cd8ec50f2eedb4a
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-08-03 11:26:09 -0500

    not the changes in this bug fix and bump versions

--- lib/HV/Monitor.pm
+++ lib/HV/Monitor.pm
@@ -11,11 +11,11 @@ HV::Monitor - A generalized module for gathering stats for a hypervisor.
 
 =head1 VERSION
 
-Version 0.0.2
+Version 0.0.3
 
 =cut
 
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 =head1 SYNOPSIS
 
